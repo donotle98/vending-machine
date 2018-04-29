53d35dc8a375f509dc5de7471929f2c8cc12b94f
Donovan T. Le <dtle647@gmail.com>
2018-04-29 11:21:06 -0700

    First checkin.

---
+++ src/test/java/VendingMachingTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class VendingMachingTest {
+
+    @Test
+    public void testCreationOfVendingMachine(){
+        VendingMachine vendingMachine = new VendingMachine();
+    }
+
+}
